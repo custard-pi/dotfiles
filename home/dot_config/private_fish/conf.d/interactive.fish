577b2764419dc22090f373419201e65812d49e13
cuatard-pi <36961256+custard-pi@users.noreply.github.com>
2026-05-06 18:03:24 +0800

    add: mcfly, fzf

--- home/dot_config/private_fish/conf.d/interactive.fish
+++ home/dot_config/private_fish/conf.d/interactive.fish
@@ -1,7 +1,25 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    # starship
-    starship init fish | source
+
+    # fish config
     # vi keybindings
     fish_vi_key_bindings
+
+    # applications
+    # fzf (disable Ctrl+R to use mcfly for history search)
+    if command -v fzf >/dev/null
+        fzf --fish | FZF_CTRL_R_COMMAND= source
+    end
+
+    # mcfly
+    if command -v mcfly >/dev/null
+        mcfly init fish | source
+        set -gx MCFLY_INTERFACE_VIEW BOTTOM
+        set -gx MCFLY_PROMPT "❯"
+    end
+
+    # starship
+    if command -v starship >/dev/null
+        starship init fish | source
+    end
 end
